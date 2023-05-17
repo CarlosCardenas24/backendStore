c9637f3c4cd24cff8c59cd21f4479dabb6d0c43f
CarlosCardenas24 <nooptions101@gmail.com>
2023-05-16 21:05:22 -0700

    Finished backend

--- Server.js
+++ Server.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')('SECRET_KEY_HERE');
+const stripe = require('stripe')('sk_test_51N36cdFFcNa2tnPmw7kAzNBi9ZgHGi0C19fpcVXkULfglPV8YqpdNuWZuytCsw1AD1ZDyEo5IuTZHK5sDNWZkMZ400MSZoRDvl');
 const express = require('express');
 const cors = require('cors')
 
@@ -16,7 +16,22 @@ app.get('/', (req, res) => {
 
 // POST method route
 app.post('/checkout', async (req, res) => {
-    res.send(req.body)
+    const newCart = req.body;
+
+    const lineItems = newCart.map((obj) => {
+        let {id, quantity} = obj
+
+        return {price: id, quantity: quantity}
+    })
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: 'http://localhost:3000/success',
+        cancel_url: 'http://localhost:3000/cancel'
+    })
+
+    res.send(JSON.stringify(session.url))
 })
 
 app.listen(4000, () => console.log('Server is up'))
