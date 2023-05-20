f8a7159f9433e0d663a90e87aed27dbb53c9e341
CarlosCardenas24 <nooptions101@gmail.com>
2023-05-19 19:36:01 -0700

    updated some urls

--- Server.js
+++ Server.js
@@ -31,8 +31,8 @@ app.post('/checkout', async (req, res) => {
     const session = await stripe.checkout.sessions.create({
         line_items: lineItems,
         mode: 'payment',
-        success_url: 'http://localhost:3000/success',
-        cancel_url: 'http://localhost:3000/cancel'
+        success_url: 'https://shopping-cart-poc-app.netlify.app/success',
+        cancel_url: 'https://shopping-cart-poc-app.netlify.app/cancel'
     })
 
     res.send(JSON.stringify(session.url))
